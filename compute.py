4cc39e414395f1a4ad3576818b8119b9ea8442b6
Naiki Shunsuke <ians2314@icloud.com>
2023-07-01 02:06:48 +0000

    change

--- compute.py
+++ compute.py
@@ -1,2 +1,18 @@
 def myadd(a, b):
+
+    if a > 0 and b > 0:
+        return a + b
+    if a < 0 and b < 0:
+        return a + b
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    
+    if a > 0 and b < 0:
+        if abs(a) > abs(b):
+            return a + b
+        if abs(a) < abs(b):
+            return a + b
+    
     return 0
